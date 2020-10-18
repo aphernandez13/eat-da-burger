db8e00f56dd2cc0aee19475bacd4dea811e908af
alfredo pedroza <alfredopedrozahernandez13@gmail.com>
2020-10-18 13:05:49 -0700

    new

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("cheddah", 0);
-INSERT INTO burgers (burger_name, devoured) VALUES ("black and blue", 0);
-INSERT INTO burgers (burger_name, devoured) VALUES ("tuna", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("brisket", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("bbq bacon", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("chicken salad", 0);
